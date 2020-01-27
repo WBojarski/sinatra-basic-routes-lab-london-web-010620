e4f0e41ccdb8182653a69bd380c13ef6730e6dff
Wojciech Bojarski <bojawojt@gmail.com>
2020-01-27 17:36:32 +0000

    Nokogiri not installing

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+class App < Sinatra:
+
+    get '/name' do 
+        "My name is Wojciech"
+    end
+
+    get '/hometown' do 
+        "My hometown is Sutton"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is.... all of them!"
+    end
 end
